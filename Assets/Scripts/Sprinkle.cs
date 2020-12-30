9c6d8076f58d1a13a72b732fa59a99a005407201
Kelly Jia <k7jia@edu.uwaterloo.ca>
2020-12-29 20:11:38 -0500

    Some comments

--- Assets/Scripts/Sprinkle.cs
+++ Assets/Scripts/Sprinkle.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Sprinkle : MonoBehaviour {
     [SerializeField] private float movementSpeed = 100f;
-    [SerializeField] private float distanceToTravel = 3;
+    [SerializeField] private float distanceToTravel = 2;
     private float distanceTravelled = 0;
 
     // Update is called once per frame
     void Update() {
-        // Keep shooting upwards, until the sprinkle goes offscreen or hits a monster
+        // Keep shooting upwards, until the sprinkle's travelled its distance
         if (distanceTravelled >= distanceToTravel) {
             Destroy(gameObject);
         } else {
@@ -20,6 +20,7 @@ public class Sprinkle : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         // If the sprinkle collides with a goose, we kill that goose!
+        // Also kill the sprinkle when it collides
         if (other.tag == "Goose") {
             other.gameObject.SetActive(false);
         }
